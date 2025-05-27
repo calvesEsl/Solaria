d129144c90860dc8896b3bc01b09768f6bd49b19
clacerdaESL <caiklacerda9@gmail.com>
2025-05-27 00:35:22 -0300

    feat: Deixei o menu Cadastro dinamico, de acordo com o tipo de cadastro do usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :is_legal_person])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :is_legal_person])
+  end
 
   private
 
